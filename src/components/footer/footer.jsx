5d82b419b26e4d3b3aead47333bdb5f08ef1a41c
Ivana Nováková <rempa@seznam.cz>
2024-06-26 19:05:26 +0200

    Rozdělení hlavní stránky na jednotlivé komponenty

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import "./footer.css"
+export const Footer = () => (
+<footer>
+<div className="container">
+  <div className="footer__content">
+    Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
+  </div>
+</div>
+</footer>
+)
